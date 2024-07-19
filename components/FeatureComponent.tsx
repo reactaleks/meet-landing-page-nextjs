b7a1e19b07c11840208db4d70bd7a9fe622cc8ae
Aleks <a.ivanov.developer@gmail.com>
2024-07-19 18:11:58 +0100

    Added LG tailwind styles for Added min width 320 to body

--- components/FeatureComponent.tsx
+++ components/FeatureComponent.tsx
@@ -2,7 +2,7 @@ export default function FeatureComponent() {
     return (
 
         <div className="w-[327px] h-[348px] md:w-[573px]  mx-auto mb-24 mt-18 flex flex-col justify-around
-        md:h-[240px] md:mb-12 md:items-center xl:mb-0
+        md:h-[240px] md:mb-12 md:items-center
         ">
             <div className="font-redhat font-black text-overline leading-overline tracking-overline text-teal uppercase text-center">Built for modern use</div>
             <h2 className="font-redhat text-[32px] text-black font-black leading-headingm text-center md:text-headingm md:leading-headingm md:w-[445px]">Smarter meetings, all in one place</h2>
--- components/HeroComponent.tsx
+++ components/HeroComponent.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function HeroComponent() {
   return (
-    <div className="grid grid-cols-12 grid-rows-12 h-[620px] xl:h-[358px] relative overflow-hidden mb-44 md:mb-56">
+    <div className="grid grid-cols-12 grid-rows-12 h-[620px] lg:h-[358px] relative overflow-hidden mb-44 md:mb-56">
       
         <Image
           src={"/desktop/image-hero-left.png"}
@@ -11,7 +11,7 @@ export default function HeroComponent() {
           height={303}
           alt=""
           className="w-[212px] h-[153px] -left-[20px] col-span-6 row-span-3 col-start-1 absolute
-          md:w-[394px] md:h-[303px] md:row-span-6 xl:-left-[25px]
+          md:w-[394px] md:h-[303px] md:row-span-6 lg:-left-[100px] xl:-left-[25px]
           "
         />
         <Image
@@ -20,19 +20,21 @@ export default function HeroComponent() {
           height={303}
           alt=""
           className="w-[212px] h-[153px] -right-[20px] col-span-6 row-span-3 col-start-7 absolute
-          md:w-[394px] md:h-[303px] md:row-span-6 xl:-right-[25px]
+          md:w-[394px] md:h-[303px] md:row-span-6 lg:-right-[100px] xl:-right-[25px]
           "
         />
 
    
         <h1 className="font-redhat text-headingm leading-headingm text-black mx-auto text-center font-black col-span-10 col-start-2 row-span-2 row-start-5
          md:col-span-6 md:col-start-4 md:row-span-2 md:row-start-8 md:text-[48px] md:leading-[48px] md:w-[339px] 
+        lg:row-start-2
         xl:row-start-3 xl:col-span-4 xl:col-start-5 xl:text-headingxl xl:w-[445px]
         ">
           Group Chat for Everyone
         </h1>
         <p className="font-redhat text-overline leading-body text-gray text-center font-medium col-span-10 col-start-2 row-span-2 row-start-7
         md:col-span-6 md:mt-2 md:col-start-4 md:row-span-1 md:row-start-10
+        lg:row-start-6 lg:col-span-4 lg:col-start-5
         xl:col-span-4 xl:col-start-5 xl:row-start-7 xl:text-body
         ">
           Meet makes it easy to connect with others face-to-face virtually and
@@ -41,6 +43,7 @@ export default function HeroComponent() {
 
         <div className="h-[132px] w-full flex flex-col justify-between items-center row-span-3 col-span-4 row-start-9 col-start-5
         md:flex-row md:justify-around md:col-span-4 md:col-start-5 md:row-start-11
+        lg:row-start-9
         xl:col-span-4 xl:col-start-5 xl:row-start-10 xl:h-[58px] xl:justify-center
         ">
         <BtnComponent btnSmall={false} btnColour="bg-teal md:mr-4" />
